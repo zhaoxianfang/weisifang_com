ee6c023175fe43c70112ace1cd1dc45ae6c5811c
zhaoxianfang <1748331509@qq.com>
2023-03-23 18:23:23 +0800

    js、css代码压缩

--- Modules/Home/Http/Controllers/Web/tools/string/CodeMinify.php
+++ Modules/Home/Http/Controllers/Web/tools/string/CodeMinify.php
@@ -3,7 +3,6 @@
 namespace Modules\Home\Http\Controllers\Web\tools\string;
 
 use Modules\Home\Http\Controllers\HomeBase;
-use zxf\min\JsMin as CodeMin;
 use zxf\min;
 
 // 代码压缩
@@ -13,15 +12,18 @@ class CodeMinify extends HomeBase
     {
         if ($this->request->isMethod('post')) {
             $codeString = $this->request->input('code');
-            if (!$codeString) {
+            if (empty($codeString)) {
                 return $this->error('请填入需要压缩的代码', 412);
             }
             // 压缩css , 也可以压缩js
-            $minifier = new min\CSS($codeString);
-            // $minifier     = new min\JS($codeString);
+            $minifier     = new min\CSS($codeString);
             $minifiedCode = $minifier->minify();
-            // $minifiedCode = CodeMin::minify($codeString);
-            return $this->success(['min_str' => $minifiedCode], '转换成功', 200);
+
+            $oldLen      = mb_strlen($codeString);
+            $newLen      = mb_strlen($minifiedCode);
+            $minifyRatio = bcmul(bcdiv($newLen, $oldLen, 4), 100, 2) . '%';
+
+            return $this->success(['min_str' => $minifiedCode, 'old_len' => $oldLen, 'new_len' => $newLen, 'minify_ratio' => $minifyRatio], '转换成功', 200);
         }
         return view('home::home.tools.string.code_minify', []);
     }
--- Modules/Home/Resources/views/home/tools/string/code_minify.blade.php
+++ Modules/Home/Resources/views/home/tools/string/code_minify.blade.php
@@ -2,7 +2,7 @@
 @extends('home::layouts.home')
 
 <!-- 设置页面标题 -->
-@section('title', 'js、css 压缩')
+@section('title', 'JS、CSS在线压缩 Code Minify')
 
 <!-- 引入当前页面的css样式文件 -->
 @section('head_css')
@@ -21,73 +21,44 @@
 <!-- 页面内容 -->
 @section('content')
 
-
-        <div class="wrapper wrapper-content  animated fadeInRight article">
-
-            <!-- <div class="row justify-content-md-center">
-            </div> -->
-
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="ibox ">
-                        <div class="ibox-title">
-                            <h1>JS、CSS代码压缩 <small>Code Minify</small></h1>
-                            <div class="ibox-tools">
-                                <!-- <a class="collapse-link">
-                                    <i class="fa fa-chevron-up"></i>
-                                </a> -->
-                                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                                    <i class="fa fa-wrench"></i>
-                                </a>
-                                <ul class="dropdown-menu dropdown-user">
-                                    <li><a href="#" class="dropdown-item">Config option 1</a>
-                                    </li>
-                                    <li><a href="#" class="dropdown-item">Config option 2</a>
-                                    </li>
-                                </ul>
-                                <!-- <a class="close-link">
-                                    <i class="fa fa-times"></i>
-                                </a> -->
-                            </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="ibox ">
+                <div class="ibox-title">
+                    <h3>JS、CSS代码压缩 <small>Code Minify</small></h3>
+                    <div class="ibox-tools">
+                        <span class="label label-warning-light float-right">:)</span>
+                    </div>
+                </div>
+                <div class="ibox-content" style="padding: 0 20px;">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h5 class="m-t-none m-b">原代码：</h5>
+                            <form role="form" method="post">
+                                <div class="form-group row">
+                                    <textarea class="form-control" name="code" placeholder="在此填入未压缩的代码..." rows="20" required></textarea>
+                                </div>
+                                <div>
+                                    <button type="submit" class="btn btn-success  dim btn-block" ><i class="fa fa-upload"></i> &nbsp&nbsp; 开始压缩</button>
+                                </div>
+                            </form>
+                            <p>使用手册：</p>
                         </div>
-                        <div class="ibox-content">
-                            <div class="row">
-                                <div class="col-sm-6 b-r">
-                                    <h3 class="m-t-none m-b">原代码：</h3>
-
-
-                                    <form role="form" method="post">
-
-
-                                       <div class="form-group row">
-                                            <textarea class="form-control" name="code" placeholder="在此填入未压缩的代码..." rows="20" required></textarea>
-                                        </div>
-
-                                        <div>
-                                            <button type="submit" class="btn btn-success  dim btn-block" ><i class="fa fa-upload"></i> &nbsp&nbsp; 开始压缩</button>
-                                        </div>
-                                    </form>
-
-                                    <p>使用手册：</p>
-
-                                </div>
-                                <div class="col-sm-6">
-                                    <h3 class="m-t-none m-b">压缩后的代码</h3>
-                                    <p class="text-center">
-                                        <div class="form-group row">
-                                            <textarea class="form-control" id="preview_string" placeholder="压缩后的代码..." rows="20"></textarea>
-                                        </div>
-                                    </p>
+                        <div class="col-sm-6">
+                            <h5 class="m-t-none m-b">压缩后的代码</h5>
+                            <div class="text-center">
+                                <div class="form-group row">
+                                    <textarea class="form-control" id="preview_string" placeholder="压缩后的代码..." rows="20"></textarea>
                                 </div>
                             </div>
+                            <p>压缩率：<text id="minify_ratio"></text></p>
                         </div>
                     </div>
                 </div>
-
             </div>
-
-
         </div>
+    </div>
+
 
 @endsection
 
@@ -104,8 +75,8 @@
             // $('#compressor_res_box').hide()
         }
         function form_after(res) {
-            console.log(res)
             $('#preview_string').val(res.data.min_str);
+            $('#minify_ratio').text(res.data.minify_ratio);
         }
 
 
