00d0b518f2a6c3cc5662ae5cba6d48fe934661bf
zhaoxianfang <1748331509@qq.com>
2023-04-02 17:28:41 +0800

    fix

--- Modules/Home/Resources/views/test.blade.php
+++ Modules/Home/Resources/views/test.blade.php
@@ -1,353 +1,264 @@
 @extends('home::layouts.home')
+
+@section('head_css')
 <style>
-    .jquery-accordion-menu, .jquery-accordion-menu * {
-        font-family: 'Open Sans', sans-serif;
-        box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        -webkit-box-sizing: border-box;
-        outline: 0
-    }
+.docs-menu-box {
+    max-width:220px;
+    width: 100%;
+    float:left;
+    position:relative;
+    /*box-shadow:0 1px 5px #333;*/
+    border: 1px solid rgba(0,0,0,.1);
 
-    .jquery-accordion-menu {
-        width: 100%;
-        float: left;
-        position: relative;
-        box-shadow: 0 20px 20px #333;
-        margin-bottom: 50px;
-    }
-
-    .jquery-accordion-menu .jquery-accordion-menu-footer, .jquery-accordion-menu .jquery-accordion-menu-header {
-        width: 100%;
-        height: 50px;
-        padding-left: 22px;
-        float: left;
-        line-height: 50px;
-        font-weight: 600;
-        color: #f0f0f0;
-        background: #414956
-    }
-
-    .jquery-accordion-menu ul {
-        margin: 0;
-        padding: 0;
-        list-style: none
-    }
-
-    .jquery-accordion-menu ul li {
-        width: 100%;
-        display: block;
-        float: left;
-        position: relative
-    }
-
-    .jquery-accordion-menu ul li a {
-        width: 100%;
-        padding: 14px 22px;
-        float: left;
-        text-decoration: none;
-        color: #f0f0f0;
-        font-size: 13px;
-        background: #414956;
-        white-space: nowrap;
-        position: relative;
-        overflow: hidden;
-        -o-transition: color .2s linear, background .2s linear;
-        -moz-transition: color .2s linear, background .2s linear;
-        -webkit-transition: color .2s linear, background .2s linear;
-        transition: color .2s linear, background .2s linear
-    }
-
-    .jquery-accordion-menu li.active > a, .jquery-accordion-menu li:hover > a {
-        color: #fff;
-        background: #3b424d;
-    }
-
-    .jquery-accordion-menu li.active > a {
-        border-left: solid 6px seagreen !important;
-        border-left-color: seagreen !important;
-    }
-
-    .jquery-accordion-menu > ul > li > a {
-        border-bottom: solid 1px #3b424d
-    }
-
-    .jquery-accordion-menu ul li a i {
-        width: 34px;
-        float: left;
-        line-height: 18px;
-        font-size: 16px;
-        text-align: left
-    }
-
-    .jquery-accordion-menu .submenu-indicator {
-        float: right;
-        right: 22px;
-        position: absolute;
-        line-height: 19px;
-        font-size: 20px;
-        -o-transition: transform .3s linear;
-        -moz-transition: transform .3s linear;
-        -webkit-transition: transform .3s linear;
-        -ms-transition: transform .3s linear
-    }
-
-    .jquery-accordion-menu ul ul.submenu .submenu-indicator {
-        line-height: 16px
-    }
-
-    .jquery-accordion-menu .submenu-indicator-minus > .submenu-indicator {
-        -ms-transform: rotate(90deg);
-        -moz-transform: rotate(90deg);
-        -webkit-transform: rotate(90deg);
-        transform: rotate(90deg)
-    }
-
-    .jquery-accordion-menu ul ul.submenu, .jquery-accordion-menu ul ul.submenu li ul.submenu {
-        width: 100%;
-        display: none;
-        position: static
-    }
-
-    .jquery-accordion-menu ul ul.submenu li {
-        clear: both;
-        width: 100%
-    }
-
-    .jquery-accordion-menu ul ul.submenu li a {
-        width: 100%;
-        float: left;
-        font-size: 11px;
-        background: #383838;
-        border-top: none;
-        position: relative;
-        border-left: solid 6px transparent;
-        -o-transition: border .2s linear;
-        -moz-transition: border .2s linear;
-        -webkit-transition: border .2s linear;
-        transition: border .2s linear
-    }
-
-    .jquery-accordion-menu ul ul.submenu li:hover > a {
-        border-left-color: #414956
-    }
-
-    .jquery-accordion-menu ul ul.submenu > li > a {
-        padding-left: 30px
-    }
-
-    .jquery-accordion-menu ul ul.submenu > li > ul.submenu > li > a {
-        padding-left: 45px
-    }
-
-    .jquery-accordion-menu ul ul.submenu > li > ul.submenu > li > ul.submenu > li > a {
-        padding-left: 60px
-    }
-
-    .jquery-accordion-menu ul li .jquery-accordion-menu-label, .jquery-accordion-menu ul ul.submenu li .jquery-accordion-menu-label {
-        min-width: 20px;
-        padding: 1px 2px 1px 1px;
-        position: absolute;
-        right: 26px;
-        top: 14px;
-        font-size: 11px;
-        font-weight: 800;
-        color: #555;
-        text-align: center;
-        line-height: 18px;
-        background: #f0f0f0;
-        border-radius: 100%
-    }
-
-    .jquery-accordion-menu ul ul.submenu li .jquery-accordion-menu-label {
-        top: 12px
-    }
-
-    .ink {
-        display: block;
-        position: absolute;
-        background: rgba(255, 255, 255, .3);
-        border-radius: 100%;
-        -webkit-transform: scale(0);
-        -moz-transform: scale(0);
-        -ms-transform: scale(0);
-        -o-transform: scale(0);
-        transform: scale(0)
-    }
-
-    .animate-ink {
-        -webkit-animation: ripple .5s linear;
-        -moz-animation: ripple .5s linear;
-        -ms-animation: ripple .5s linear;
-        -o-animation: ripple .5s linear;
-        animation: ripple .5s linear
-    }
-
-    @-webkit-keyframes ripple {
-        100% {
-            opacity: 0;
-            -webkit-transform: scale(2.5)
-        }
-    }
-
-    @-moz-keyframes ripple {
-        100% {
-            opacity: 0;
-            -moz-transform: scale(2.5)
-        }
-    }
-
-    @-o-keyframes ripple {
-        100% {
-            opacity: 0;
-            -o-transform: scale(2.5)
-        }
-    }
-
-    @keyframes ripple {
-        100% {
-            opacity: 0;
-            transform: scale(2.5)
-        }
-    }
-
-    .blue.jquery-accordion-menu .jquery-accordion-menu-footer, .blue.jquery-accordion-menu .jquery-accordion-menu-header, .blue.jquery-accordion-menu ul li a {
-        background: #4A89DC
-    }
-
-    .blue.jquery-accordion-menu > ul > li.active > a, .blue.jquery-accordion-menu > ul > li:hover > a {
-        background: #3e82da
-    }
-
-    .blue.jquery-accordion-menu > ul > li > a {
-        border-bottom-color: #3e82da
-    }
-
-    .blue.jquery-accordion-menu ul ul.submenu li:hover > a {
-        border-left-color: #3e82da
-    }
-
-    .green.jquery-accordion-menu .jquery-accordion-menu-footer, .green.jquery-accordion-menu .jquery-accordion-menu-header, .green.jquery-accordion-menu ul li a {
-        background: #03A678
-    }
-
-    .green.jquery-accordion-menu > ul > li.active > a, .green.jquery-accordion-menu > ul > li:hover > a {
-        background: #049372
-    }
-
-    .green.jquery-accordion-menu > ul > li > a {
-        border-bottom-color: #049372
-    }
-
-    .green.jquery-accordion-menu ul ul.submenu li:hover > a {
-        border-left-color: #049372
-    }
-
-    .red.jquery-accordion-menu .jquery-accordion-menu-footer, .red.jquery-accordion-menu .jquery-accordion-menu-header, .red.jquery-accordion-menu ul li a {
-        background: #ED5565
-    }
-
-    .red.jquery-accordion-menu > ul > li.active > a, .red.jquery-accordion-menu > ul > li:hover > a {
-        background: #DA4453
-    }
-
-    .red.jquery-accordion-menu > ul > li > a {
-        border-bottom-color: #DA4453
-    }
-
-    .red.jquery-accordion-menu ul ul.submenu li:hover > a {
-        border-left-color: #DA4453
-    }
-
-    .white.jquery-accordion-menu .jquery-accordion-menu-footer, .white.jquery-accordion-menu .jquery-accordion-menu-header, .white.jquery-accordion-menu ul li a {
-        background: #fff;
-        color: #555
-    }
-
-    .white.jquery-accordion-menu > ul > li.active > a, .white.jquery-accordion-menu > ul > li:hover > a {
-        background: #f0f0f0
-    }
-
-    .white.jquery-accordion-menu > ul > li > a {
-        border-bottom-color: #f0f0f0
-    }
-
-    .white.jquery-accordion-menu ul ul.submenu li:hover > a {
-        border-left-color: #f0f0f0
-    }
-
-    .white.jquery-accordion-menu ul ul.submenu li a {
-        color: #f0f0f0
-    }
-
-    .white.jquery-accordion-menu > ul > li > a > .ink {
-        background: rgba(0, 0, 0, .1)
-    }
-
-    .black.jquery-accordion-menu .jquery-accordion-menu-footer, .black.jquery-accordion-menu .jquery-accordion-menu-header, .black.jquery-accordion-menu ul li a {
-        background: #292929
-    }
-
-    .black.jquery-accordion-menu > ul > li.active > a, .black.jquery-accordion-menu > ul > li:hover > a {
-        background: #222
-    }
-
-    .black.jquery-accordion-menu > ul > li > a {
-        border-bottom-color: #222
-    }
-
-    .black.jquery-accordion-menu ul ul.submenu li:hover > a {
-        border-left-color: #222
-    }
+}
+.docs-menu-box .docs-menu-box-footer,.docs-menu-box .docs-menu-box-header {
+    width:100%;
+    height:50px;
+    padding-left:22px;
+    float:left;
+    line-height:50px;
+    font-weight:600;
+    color:#f0f0f0;
+    background:#414956
+}
+.docs-menu-box ul {
+    margin:0;
+    padding:0;
+    list-style:none
+}
+.docs-menu-box ul li {
+    width:100%;
+    display:block;
+    float:left;
+    position:relative
+}
+.docs-menu-box ul li a {
+    width:100%;
+    padding:14px 22px;
+    float:left;
+    text-decoration:none;
+    color:#f0f0f0;
+    font-size:13px;
+    background:#414956;
+    white-space:nowrap;
+    position:relative;
+    overflow:hidden;
+}
+.docs-menu-box>ul>li.active>a,.docs-menu-box>ul>li:hover>a {
+    color:#fff;
+    background:#3b424d
+}
+.docs-menu-box>ul>li>a {
+    border-bottom:solid 1px #3b424d
+}
+.docs-menu-box ul li a i {
+    width:34px;
+    float:left;
+    line-height:18px;
+    font-size:16px;
+    text-align:left
+}
+.docs-menu-box ul ul.submenu,.docs-menu-box ul ul.submenu li ul.submenu {
+    width:100%;
+    display:none;
+    position:static
+}
+.docs-menu-box ul ul.submenu li {
+    clear:both;
+    width:100%
+}
+.docs-menu-box ul ul.submenu li a {
+    width:100%;
+    float:left;
+    font-size:11px;
+    background:#383838;
+    border-top:none;
+    position:relative;
+    border-left:solid 6px transparent;
+}
+.docs-menu-box.white ul ul.submenu li a{
+    background: #fff;
+    color: #555!important;
+}
+.docs-menu-box ul ul.submenu li:hover>a {
+    border-left-color:#414956
+}
+.docs-menu-box ul ul.submenu>li>a {
+    padding-left:30px
+}
+.docs-menu-box ul ul.submenu>li>ul.submenu>li>a {
+    padding-left:45px
+}
+.docs-menu-box ul ul.submenu>li>ul.submenu>li>ul.submenu>li>a {
+    padding-left:60px
+}
+.docs-menu-box ul li .docs-menu-box-label,.docs-menu-box ul ul.submenu li .docs-menu-box-label {
+    min-width:20px;
+    padding:1px 2px 1px 1px;
+    position:absolute;
+    right:18px;
+    top:14px;
+    font-size:11px;
+    font-weight:800;
+    color:#555;
+    text-align:center;
+    line-height:18px;
+    background:#f0f0f0;
+    border-radius:100%
+}
+.docs-menu-box ul ul.submenu li .docs-menu-box-label {
+    top:12px
+}
+.white.docs-menu-box .docs-menu-box-footer,.white.docs-menu-box .docs-menu-box-header,.white.docs-menu-box ul li a {
+    background:#fff;
+    color:#555
+}
+.white.docs-menu-box>ul>li.active>a,.white.docs-menu-box>ul>li:hover>a {
+    background:#f0f0f0
+}
+.white.docs-menu-box>ul>li>a {
+    border-bottom-color:#f0f0f0
+}
+.white.docs-menu-box ul ul.submenu li:hover>a {
+    border-left-color:#f0f0f0
+}
+.white.docs-menu-box ul ul.submenu li a {
+    color:#f0f0f0
+}
+.black.docs-menu-box .docs-menu-box-footer,.black.docs-menu-box .docs-menu-box-header,.black.docs-menu-box ul li a {
+    background:#292929
+}
+.black.docs-menu-box>ul>li.active>a,.black.docs-menu-box>ul>li:hover>a {
+    background:#222
+}
+.black.docs-menu-box>ul>li>a {
+    border-bottom-color:#222
+}
+.black.docs-menu-box ul ul.submenu li:hover>a {
+    border-left-color:#222
+}
 
 </style>
 <style>
 /*自定义箭头*/
-    .custom-arrow{
-        float: right;
-        right: 22px;
-        position: absolute;
-        line-height: 19px;
-        font-size: 20px;
-        width: 8px;
-        height: 8px;
-    }
-    .custom-right-arrow {
-        border-top: 2px solid;
-        border-right: 2px solid;
-        border-color: #ccc;
-        content: '';
-        position: absolute;
-        right: 11px;
-        top: 20px;
-        transform: rotate(45deg);
-    }
+.custom-arrow{
+    float: right;
+    right: 22px;
+    position: absolute;
+    line-height: 19px;
+    font-size: 20px;
+    width: 8px;
+    height: 8px;
+}
+.custom-right-arrow {
+    border-top: 2px solid;
+    border-right: 2px solid;
+    border-color: #ccc;
+    content: '';
+    position: absolute;
+    right: 9px;
+    top: 20px;
+    transform: rotate(45deg);
+}
 
-    .custom-down-arrow {
-        border-top: 2px solid;
-        border-right: 2px solid;
-        border-color: #ccc;
-        content: '';
+.custom-down-arrow {
+    border-top: 2px solid;
+    border-right: 2px solid;
+    border-color: #ccc;
+    content: '';
+    position: absolute;
+    right: 7px;
+    top: 20px;
+    transform: rotate(135deg);
+}
+ /*定义左边布局*/
+.docs-box{
+    display:flex;
+    width: 100%;
+}
+.docs-left-menu{
+    flex:0 0 220px;
+    /*margin-bottom: 50px;*/
+    overflow: auto;
+    height: calc( 100vh - 102px );
+    background-color: #fff;
+
+    /* 设置左右方向显示隐藏 */
+    /*height: 100%;*/
+    z-index: 100;
+    transition: all 0.5s;
+}
+@media screen and (max-width:768px){
+    /*.docs-left-menu{*/
+    /*    flex:0 0 0px;*/
+    /*}*/
+    .docs-left-menu{
         position: absolute;
-        right: 11px;
-        top: 20px;
-        transform: rotate(135deg);
     }
+    .docs-menu-btn-nav{
+        position: absolute!important;
+        top: 30vh!important;
+        transition: all 0.5s;
+    }
+    .show-menu.docs-menu-btn-nav{
+        left: 220px!important;
+    }
+    .top-navigation .container{
+        padding: 0;
+    }
+}
+@media screen and (min-width:768px){
+    .docs-menu-btn-nav{
+        /*display: none;*/
+    }
+}
+.docs-right-content{
+    flex: 1 1;
+    border: 1px solid green;
+    padding: 0 20px;
+}
+.docs-menu-btn-nav.show-menu {
+    background-image: url({{ asset('static/images/nuv_03.png') }});
+}
+.docs-menu-btn-nav.hidden-menu {
+    background-image: url({{ asset('static/images/nuv_04.png') }});
+}
+
+.docs-menu-btn-nav {
+    position: sticky;
+    left: 0;
+    top: 40%;
+    width: 25px;
+    height: 93px;
+    border-style: none;
+    background: url({{ asset('static/images/nuv_03.png') }}) no-repeat transparent;
+    outline: none;
+    border-left: 1px solid #fff;
+    margin-left: -1px;
+    z-index: 100;
+}
+
+
 </style>
+@endsection
+
 @section('page_js')
     <!-- 页面中引入page js -->
     <script type="text/javascript">
         jQuery(document).ready(function () {
 
-            $(".jquery-accordion-menu a").siblings("ul.submenu").each(function(index, domele) {
+            $(".docs-menu-box a").siblings("ul.submenu").each(function(index, domele) {
                 // 追加一个有子元素的箭头元素
                 $(domele).siblings("a").append('<span class="custom-arrow custom-right-arrow"></span>');
             })
             // 点击时候修改箭头方向和切换展示子元素
-            jQuery(".jquery-accordion-menu a").click(function () {
+            jQuery(".docs-menu-box a").click(function () {
                 var submenu = $(this).siblings("ul.submenu");
                 if (submenu.length > 0) {
                     var submenuShow = submenu.css("display");
-                    submenu.css("display", submenuShow == "block" ? "none" : "block");
+                    // submenu.css("display", submenuShow == "block" ? "none" : "block");
+                    // 展开收起效果
+                    submenuShow == "block"? $(submenu).slideUp(600):$(submenu).slideDown(600);
 
                     if (submenuShow == "block") {
                         $(this).find(".custom-arrow").removeClass("custom-down-arrow");
@@ -358,15 +269,39 @@
                     }
                 }
             });
+
+            function bindClickDocsMenu(){
+                // open hidden
+                $('.hidden-menu.docs-menu-btn-nav').off().on('click', function (e) {
+                    console.log('open')
+                    $('.docs-left-menu').css({ 'flex':'0 0 220px','left':'0' })
+                    $(this).css({'left':'220px' })
+                    $(this).removeClass('hidden-menu').addClass('show-menu')
+                    // $('.docs-menu-box').css({ 'left': 0 })
+                    // document.body.style.overflow = 'hidden' //禁止滑动
+                    bindClickDocsMenu();
+                })
+                $('.show-menu.docs-menu-btn-nav').off().on('click', function (e) {
+                    console.log('hidden')
+                    // $('.docs-menu-box').css({ 'left': -50 + 'vw' })
+                    $('.docs-left-menu').css({ 'flex':'0 0 0px','left':'-220px' })
+                    $(this).css({ 'left':'0' })
+                    $(this).removeClass('show-menu').addClass('hidden-menu')
+                    // document.body.style.overflow = 'visible'  //关闭抽屉,解除禁止滑动
+                    bindClickDocsMenu();
+                })
+            }
+            bindClickDocsMenu();
         });
     </script>
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-3">
-            <div id="jquery-accordion-menu" class="jquery-accordion-menu">
-                <div class="jquery-accordion-menu-header">
+    <div class="docs-box docs-left-show-menu">
+        <div class="docs-left-menu">
+            {{-- white 或者默认黑色 --}}
+            <div class="docs-menu-box white">
+                <div class="docs-menu-box-header">
                     Header
                 </div>
                 <ul>
@@ -390,7 +325,7 @@
                             </i>
                             Gallery
                         </a>
-                        <span class="jquery-accordion-menu-label">
+                        <span class="docs-menu-box-label">
 							12
 						</span>
                     </li>
@@ -399,7 +334,7 @@
                             <i class="fa fa-cog">
                             </i>
                             Services
-                            <span class="jquery-accordion-menu-label">+1</span>
+                            <span class="docs-menu-box-label">+1</span>
                             <span class="custom-arrow custom-right-arrow"></span>
                         </a>
                         <ul class="submenu">
@@ -470,7 +405,7 @@
                                 <a href="#">
                                     Graphics
                                 </a>
-                                <span class="jquery-accordion-menu-label">
+                                <span class="docs-menu-box-label">
 									10
 								</span>
                             </li>
@@ -501,13 +436,17 @@
                         </a>
                     </li>
                 </ul>
-                <div class="jquery-accordion-menu-footer">
-                    Footer
+                <div class="docs-menu-box-footer test-close">
+                    Footer  test Close
                 </div>
             </div>
         </div>
-        <div class="col-lg-9">
-            content
+        <button type="button" class="show-menu docs-menu-btn-nav"></button>
+        <div class="docs-right-content" style="height: 8000px">
+            <div class="row">
+                content-row
+                https://www.jq22.com/yanshi22165
+            </div>
         </div>
     </div>
 @endsection
