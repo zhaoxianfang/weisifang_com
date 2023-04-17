8e3e635a792f71632869dbd521e1f414cf7f565a
zhaoxianfang <1748331509@qq.com>
2023-04-17 17:48:07 +0800

    fix

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,28 @@ return [
     |
     */
 
+    /* 你可以为 1 个或多个路径启用 CORS;例如 ['api/*'] */
     'paths' => ['api/*', 'tools/*', 'sanctum/csrf-cookie'],
 
+    /* 匹配请求方法 */
     'allowed_methods' => ['*'],
 
+    /* 匹配请求源; 可以使用通配符，例如 * 或 *.mydomain.com 或 mydomain.com:* */
     'allowed_origins' => ['*'],
 
+    /* 使用 preg_match 匹配请求源 */
     'allowed_origins_patterns' => [],
 
+    /* 设置 Access-Control-Allow-Headers 响应头 */
     'allowed_headers' => ['*'],
 
+    /* 设置 Access-Control-Expose-Headers 响应头 */
     'exposed_headers' => [],
 
+    /* 设置 Access-Control-Max-Age 响应头 */
     'max_age' => 0,
 
+    /* 设置 Access-Control-Allow-Credentials 响应头 */
     'supports_credentials' => false,
 
 ];
