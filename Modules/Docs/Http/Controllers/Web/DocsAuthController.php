5fc5662392b107296445f5201244ad1b6d30ecdd
zhaoxianfang <1748331509@qq.com>
2023-05-08 22:42:46 +0800

    fix

---
+++ Modules/Docs/Http/Controllers/Web/DocsAuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Modules\Docs\Http\Controllers\Web;
+
+use Modules\Docs\Http\Controllers\DocsBaseController;
+use Modules\System\Http\Requests\WebLoginAuthRequest;
+
+class DocsAuthController extends DocsBaseController
+{
+
+    /**
+     * 登录页面
+     */
+    public function login()
+    {
+        if (!auth('web')->guest()) {
+            return to_route('docs.home');
+        }
+        return view('docs::web/login');
+    }
+
+    public function loginHandle(WebLoginAuthRequest $request)
+    {
+        $credentials = $request->only(['mobile', 'password']);;
+        $remember = true; // 是否记住密码
+        if (!auth('web')->attempt($credentials, $remember)) {
+            return response()->json(['error' => '账号或者密码错误'], 401);
+        }
+
+        $jump = url()->previous(); // 成功后跳转到上一个页面地址
+        return $this->success([], '登录成功', 200, $jump);
+    }
+
+    /**
+     * 注册
+     */
+    public function register()
+    {
+        return view('docs::web/register');
+    }
+
+    public function qqlogin()
+    {
+        $url = url('docs/auth/callback');
+        return to_route('callback.tencent.login', ['callback_url' => urlencode($url)], 302);
+    }
+
+    public function weibologin()
+    {
+        $url = url('docs/auth/callback');
+        return to_route('callback.weibo.login', ['callback_url' => urlencode($url)], 302);
+    }
+
+    // 登录回调
+    public function loggedIn()
+    {
+        $user = collect(request()->post())->except(['sys'])->toArray();
+
+        $remember = false; // 是否记住密码
+        if (!auth('web')->loginUsingId($user['id'], $remember)) {
+            return response()->json(['error' => '账号或者密码错误'], 401);
+        }
+
+        return to_route('docs.home', [], 302);
+
+    }
+}
--- Modules/Docs/Routes/web.php
+++ Modules/Docs/Routes/web.php
@@ -17,10 +17,26 @@ use \Modules\Docs\Http\Controllers\Web;
 Route::prefix('docs')->name('docs')->group(function () {
     // 文档主页
     Route::get('/', [Web\DocsController::class, 'index'])->name('docs_home');
-    // 创建文档
-    Route::get('create', [Web\DocsAppController::class, 'create'])->name('create');
-    Route::post('create', [Web\DocsAppController::class, 'store'])->name('store');
-    // 某文档首页
-    Route::get('{docsApp}', [Web\DocsAppController::class, 'firstPage'])->name('doc_first_page');
+    // apps 应用管理
+    Route::prefix('')->group(function () {
+        // 创建文档
+        Route::get('create', [Web\DocsAppController::class, 'create'])->name('create');
+        Route::post('create', [Web\DocsAppController::class, 'store'])->name('store');
+        // 某文档首页
+        Route::get('{docsApp}', [Web\DocsAppController::class, 'firstPage'])->name('doc_first_page');
+    });
 
+    // 登录注册授权页
+    Route::prefix('auth')->name('auth.')->group(function () {
+        // 登录页
+        Route::get('login', [Web\DocsAuthController::class,'login'])->name('login');
+        Route::post('login', [Web\DocsAuthController::class,'loginHandle']);
+        Route::get('register', [Web\DocsAuthController::class,'register'])->name('register');
+        Route::post('register', [Web\DocsAuthController::class,'registerHandle']);
+
+        Route::get('qqlogin', [Web\DocsAuthController::class,'qqlogin'])->name('qqlogin');
+        Route::get('weibologin', [Web\DocsAuthController::class,'weibologin'])->name('weibologin');
+
+        Route::post('callback', [Web\DocsAuthController::class,'loggedIn']);
+    });
 });
